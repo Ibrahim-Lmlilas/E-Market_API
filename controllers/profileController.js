d5603ba0ba6ab16bae081ccc6fab4079fd2f0e59
fouad lmrini <fouad11.lamrini@gmail.com>
2025-10-14 12:10:58 +0100

    function changePassword

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -40,5 +40,40 @@ class ProfileController {
       });
     }
   }
+
+    async changePassword(req, res) {
+    try {
+      const { oldPassword, newPassword, confirmPassword } = req.body;
+
+      if (!oldPassword || !newPassword || !confirmPassword) {
+        return res.status(400).json({ success: false, message: 'Please provide old, new, and confirm password' });
+      }
+
+      if (newPassword !== confirmPassword) {
+        return res.status(400).json({ success: false, message: 'New password and confirm password do not match' });
+      }
+
+      const user = await User.findById(req.user._id);
+      if (!user) return res.status(404).json({ success: false, message: 'User not found' });
+      const isMatch = await bcrypt.compare(oldPassword, user.password);
+      if (!isMatch) {
+        return res.status(401).json({ success: false, message: 'Old password is incorrect' });
+      }
+
+      
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+
+      await user.save();
+
+      return res.status(200).json({ success: true, message: 'Password updated successfully ✅' });
+
+    } catch (error) {
+      console.error('Error changing password:', error);
+      return res.status(500).json({ success: false, message: error.message || 'Server Error' });
+    }
+  }
+
+
 }
 module.exports =  ProfileController;
