8524170789c4844ed633286898a7ba741942be7d
fouad lmrini <fouad11.lamrini@gmail.com>
2025-10-14 10:06:58 +0100

    ajouter files profileRoute profileController

---
+++ controllers/profileController.js
@@ -0,0 +1,6 @@
+const User = require('../models/User');
+const Role = require('../models/Role');
+const jwt = require('jsonwebtoken');
+class ProfileController{
+    
+}
--- server.js
+++ server.js
@@ -46,6 +46,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
 app.use('/api/auth', authRoutes);
 app.use('/api/categories', categoryRoutes);
 app.use('/api/products', productRoutes);
+app.use('/api/profiles', profileRoutes);
 
 
 //----------------------------------------------------------
