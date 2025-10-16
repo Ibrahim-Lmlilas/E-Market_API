fbe04dc129d203932f2a050e38283be17eedd0d8
Younes <younes.imouga@gmail.com>
2025-10-16 15:24:00 +0100

    finished coupons part

--- services/CartService.js
+++ services/CartService.js
@@ -12,6 +12,30 @@ class CartService {
         const cart = await Cart.findOne({ user_id: userId });
         return cart;
     }
+
+    async applyCoupon(code, cartId) {
+        const cart = await Cart.findOne({ _id: cartId });
+        if (cart) {
+            
+            if (cart.coupon) {
+                cart.coupon = cart.coupon + ',' + code;
+            } else {
+                cart.coupon = code;
+            }
+
+            await cart.save();
+            return cart;
+        }
+    }
+
+    async getApliedCoupons(cardId) {
+        const cart = await Cart.findOne({ _id: cardId, isDeleted: false });
+        if(!cart){
+            return null;
+        }
+        let allCoupons = cart.coupon.split(',');
+        return allCoupons;
+    }
 }
 
 module.exports = new CartService();
