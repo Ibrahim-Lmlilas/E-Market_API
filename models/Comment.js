d77ec4f9ba48d5938bae0643a4c154b8a826d8a2
fouad lmrini <fouad11.lamrini@gmail.com>
2025-10-19 22:52:04 +0100

    model comment

---
+++ models/Comment.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+      maxlength: 500,
+    },
+  },
+  { timestamps: true }
+);
+
+// 
+commentSchema.index({ user: 1, product: 1 }, { unique: true });
+
+module.exports = mongoose.model('Comment', commentSchema);
