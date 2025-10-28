09bcfb1516b041d55402f3df5fe3180ba384f0df
Younes <younes.imouga@gmail.com>
2025-10-29 00:14:23 +0100

    fixed coupon errors

--- routes/couponRoutes.js
+++ routes/couponRoutes.js
@@ -3,14 +3,14 @@ const router = express.Router();
 const { protect, adminOnly } = require('../middlewares/auth');
 const couponController = require('../controllers/couponController');
 const validator = require('../middlewares/validationMiddleware');
-const { couponSchema } = require('../utils/validationSchema');
+const { couponSchema, editCouponSchema } = require('../utils/validationSchema');
 
 router.get('/', protect, adminOnly, couponController.getAllCoupons);
 router.get('/:id', protect, adminOnly, couponController.getCouponById);
 
 router.post('/', protect, adminOnly, validator.validate(couponSchema), couponController.createCoupon);
 
-router.put('/:id', protect, adminOnly, validator.validate(couponSchema), couponController.updateCoupon);
+router.put('/:id', protect, adminOnly, validator.validate(editCouponSchema), couponController.updateCoupon);
 
 router.delete('/:id', protect, adminOnly, couponController.deleteCoupon);
 
--- utils/validationSchema.js
+++ utils/validationSchema.js
@@ -147,7 +147,17 @@ const couponSchema = yup.object({
     category_id: yup.string().required('Category is required'),
     user_id: yup.string().required('User is required'),
     usesLeft: yup.number().required('Maximum Uses is required'),
-})
+});
+
+const editCouponSchema = yup.object({
+    code: yup.string(),
+    type: yup.string(),
+    discount: yup.number(),
+    expirationDate: yup.date(),
+    category_id: yup.string(),
+    user_id: yup.string(),
+    usesLeft: yup.number(),
+});
 
 module.exports = {
     userSchema, 
@@ -161,5 +171,6 @@ module.exports = {
     CartItemSchema,
     UpdateCartItemSchema,
     passwordSchema,
-    couponSchema
+    couponSchema,
+    editCouponSchema
 };
