93ffc292a0de178dd957b81a5530d989916c9735
fouad lmrini <fouad11.lamrini@gmail.com>
2025-10-30 12:09:56 +0100

    install package compression

--- config/redisClient.js
+++ config/redisClient.js
@@ -4,14 +4,14 @@ const redisClient = redis.createClient({
   url: process.env.REDIS_URL || 'redis://localhost:6379',
 });
 
-redisClient.on('error', (err) => console.error('❌ Redis Error:', err));
+redisClient.on('error', (err) => console.error(' Redis Error:', err));
 
 (async () => {
   try {
     await redisClient.connect();
-    console.log('✅ Connected to Redis');
+    console.log(' Connected to Redis');
   } catch (err) {
-    console.error('❌ Redis connection failed:', err);
+    console.error(' Redis connection failed:', err);
   }
 })();
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const compression = require('compression');
 const express = require('express');
 const mongoose = require('mongoose');
 const swaggerUi = require('swagger-ui-express');
