e24527d44a125c8d883c33cd511055e469aaf5b2
Younes <younes.imouga@gmail.com>
2025-10-30 09:26:08 +0100

    worked on order need to work on apply coupon

--- controllers/couponController.js
+++ controllers/couponController.js
@@ -1,4 +1,4 @@
-const CouponService = require("../services/couponService");
+const CouponService = require("../services/CouponService");
 const Coupon = require("../models/Coupon");
 const NotificationService = require('../services/NotificationService');
 
