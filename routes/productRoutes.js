2134fb6824fe716a8a3f839f7c7166a57c57c6e6
Ibrahim-Lmlilas <lmlilas.ibrahim@student.youcode.ma>
2025-11-03 09:55:42 +0100

    fix

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -16,7 +16,7 @@ const { cache } = require('../middlewares/CachingMiddleware');
  * @swagger
  * /api/products/v1:
  *   get:
- *     summary: Get all published products (Public) with pagination
+ *     summary: Get all published test products (Public) with pagination
  *     tags: [Products]
  *     parameters:
  *       - in: query
