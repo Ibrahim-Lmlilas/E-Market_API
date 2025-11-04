6d7729714e9910a80c2c02c44542d42088359d78
Younes <younes.imouga@gmail.com>
2025-11-04 16:23:40 +0100

    nothing

--- server.js
+++ server.js
@@ -70,7 +70,7 @@ app.get('/fouad', (req, res) => {
 // ---------------------Routes--------------------------
 app.get('/', (req, res) => {
   res.json({
-    message: 'Welcome to E-Market API Ci/Cd working',
+    message: 'Welcome to E-Market API',
     status: 'Server is running',
     version: '1.0.0',
     documentation: 'http://localhost:3000/api-docs',
